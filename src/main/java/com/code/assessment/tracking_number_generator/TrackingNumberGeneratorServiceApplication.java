f4773b099088faff8aa28adcd302f300be7e0cd2
ramsahnp <ramsahnp@gmail.com>
2025-05-21 13:02:20 +0530

    initial adding project source code

---
+++ src/main/java/com/code/assessment/tracking_number_generator/TrackingNumberGeneratorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.code.assessment.tracking_number_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingNumberGeneratorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingNumberGeneratorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/assessment/tracking_number_generator/TrackingNumberGeneratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.assessment.tracking_number_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingNumberGeneratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
